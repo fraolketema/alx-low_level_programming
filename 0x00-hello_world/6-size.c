78a019848e66cbff19e8aeb0e014b64956284a67
fraolketema <ketemafiraol94@gmail.com>
2023-08-10 16:47:57 -0700

    Task 6 Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 /**
- * main - Size is not grandeur
- *
- * Return: Always 0 "Success"
+ * main- This is the main function.
+ * Return: 0 when successful
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-
-printf("Size of a char: %lu byte(s)\n", sizeof(a));
-printf("size of int: %lu byte(s)\n", sizeof(b));
-printf("size of a long int: %lu byte(s)\n", sizeof(c));
-printf("size of long long int: %lu byte(s)\n", sizeof(d));
-printf("size of a float: %lu byte(s)\n", sizeof(e));
-return (0);
+	printf("Size of a char: %zu byte\(s\)\n", sizeof(char));
+	printf("Size of an int: %zu byte\(s\)\n", sizeof(int));
+	printf("Size of a long int: %zu byte\(s\)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte\(s\)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte\(s\)\n", sizeof(float));
+	return (0);
 }
+
