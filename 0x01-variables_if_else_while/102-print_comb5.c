633271a96f04e3b9298d7464469d0271e4d02f01
fraolketema <ketemafiraol94@gmail.com>
2023-08-11 17:12:23 -0700

    Task 12 print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 int tens1, ones1, tens2, ones2;
+int firstcombination = 1;
 
 for (tens1 = 0; tens1 < 10; tens1++)
 {
@@ -20,15 +21,19 @@ for (ones2 = 0; ones2 < 10; ones2++)
 {
 if (tens1 * 10 + ones1 <= tens2 * 10 + ones2)
 {
+if (tens1 * 10 + ones1 < tens2 * 10 + ones2)
+{
+if (!firstcombination)
+{
+putchar(',');
+putchar(' ');
+}
 putchar(tens1 + '0');
 putchar(ones1 + '0');
 putchar(' ');
 putchar(tens2 + '0');
 putchar(ones2 + '0');
-if (!(tens1 == 9 && ones1 == 9 && tens2 == 9 && ones2 == 9))
-{
-putchar(',');
-putchar(' ');
+firstcombination = 0;
 }
 }
 }
