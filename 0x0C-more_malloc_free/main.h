a8d734db36918e6243612f471a4739a152541a0b
fraolketema <ketemafiraol94@gmail.com>
2023-09-07 14:07:52 -0700

    Task 4 A function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <limits.h>
 
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
